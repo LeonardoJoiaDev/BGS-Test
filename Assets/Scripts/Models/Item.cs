70923fe3e5274c96dc60b3eeea948dd162baff62
LeonardoJoiaDev <leonardojoiadev@gmail.com>
2024-03-07 20:50:51 -0300

    Add Item model and SO itens

---
+++ Assets/Scripts/Models/Item.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item")]
+public class Item: ScriptableObject
+{
+    public Sprite sprite;
+    public float value;
+}
+
---
+++ Assets/Scripts/Models/ItensLibrary.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItensLibrary : MonoBehaviour
+{
+    public List<Item> headItens = new List<Item>();
+    public List<Item> armItens = new List<Item>();
+    public List<Item> hoodItens = new List<Item>();
+
+}
